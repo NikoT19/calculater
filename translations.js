496904f0ad1d375436c0739c7564343e2a5edb09
NikoT19 <77738198+NikoT19@users.noreply.github.com>
2025-01-09 16:38:10 +0600

    Update translations.js

--- translations.js
+++ translations.js
@@ -88,3 +88,14 @@ const translations = {
         totalRisk: "ความเสี่ยงทั้งหมด (%)"
     }
 };
+function changeLanguage() {
+    const selectedLanguage = document.getElementById('language').value;
+    const elementsToTranslate = document.querySelectorAll('[data-lang]');
+
+    elementsToTranslate.forEach(element => {
+        const key = element.getAttribute('data-lang');
+        if (translations[selectedLanguage][key]) {
+            element.textContent = translations[selectedLanguage][key];
+        }
+    });
+}
